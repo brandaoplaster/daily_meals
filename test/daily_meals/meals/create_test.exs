1c654012fcc8296913437d75a49fdcb26bb89d87
brandaoplaster <brandaoplaster@gmail.com>
2021-06-06 20:24:16 -0300

    chore: Adding tests for meal create

---
+++ test/daily_meals/meals/create_test.exs
@@ -0,0 +1,39 @@
+defmodule DailyMeals.Meals.CreateTest do
+  use DailyMeals.DataCase
+
+  alias DailyMeals.Error
+
+  describe "call/1" do
+    test "when all params are valid, returns the meal" do
+      params = %{
+        calories: 20,
+        date: ~N[2021-05-02 12:00:00],
+        description: "coffee"
+      }
+
+      response = DailyMeals.create_meal(params)
+
+      expected_response = %{
+        calories: 20,
+        date: ~N[2021-05-02 12:00:00],
+        description: "coffee"
+      }
+
+      assert {:ok, expected_response} = response
+    end
+
+    test "when there are invalid params, returns an error" do
+      params = %{
+        calories: 20,
+        date: ~N[2001-05-02 12:00:00]
+      }
+
+      response = DailyMeals.create_meal(params)
+
+      expected_response = %{description: ["can't be blank"]}
+
+      assert {:error, %Error{status: :bad_request, message: changeset}} = response
+      assert errors_on(changeset) == expected_response
+    end
+  end
+end
