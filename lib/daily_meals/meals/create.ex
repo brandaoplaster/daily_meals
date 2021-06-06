c660b6d5831da5d2b355afefb0a7da55da51c731
brandaoplaster <brandaoplaster@gmail.com>
2021-06-06 18:39:44 -0300

    fix: Fixing meal create module handle errors

--- lib/daily_meals/meals/create.ex
+++ lib/daily_meals/meals/create.ex
@@ -1,9 +1,16 @@
 defmodule DailyMeals.Meals.Create do
-  alias DailyMeals.{Meal, Repo}
+  alias DailyMeals.{Error, Meal, Repo}
 
   def call(params) do
     params
     |> Meal.changeset()
     |> Repo.insert()
+    |> handle_insert()
+  end
+
+  defp handle_insert({:ok, %Meal{}} = meal), do: meal
+
+  defp handle_insert({:error, changeset}) do
+    {:error, Error.build(:bad_request, changeset)}
   end
 end
