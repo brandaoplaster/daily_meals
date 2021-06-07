4a1754737fef48c3aa404cc7f0619ec676773191
brandaoplaster <brandaoplaster@gmail.com>
2021-06-06 23:18:40 -0300

    chore: Adding tests for meal get

---
+++ test/daily_meals/meals/get_test.exs
@@ -0,0 +1,35 @@
+defmodule DailyMeals.Meals.GetTest do
+  use DailyMeals.DataCase
+
+  alias DailyMeals.Error
+
+  describe "Get Meal" do
+    test "when a valid id is given, returns the meal" do
+      params = %{
+        calories: 20,
+        date: ~N[2021-05-02 12:00:00],
+        description: "Coffee"
+      }
+
+      {_ok, meal} = DailyMeals.create_meal(params)
+
+      response = DailyMeals.get_meal_by_id(meal.id)
+
+      expected_response = %{
+        calories: 20,
+        date: ~N[2021-05-02 12:00:00],
+        description: "Coffee"
+      }
+
+      assert {:ok, expected_response} = response
+    end
+
+    test "when an invalid id is given, returns an error" do
+      id = "ce9185ee-dc7e-4751-9e1d-53449696fb33"
+
+      response = DailyMeals.get_meal_by_id(id)
+
+      assert {:error, %Error{message: "Meal not found", status: :not_found}} = response
+    end
+  end
+end
