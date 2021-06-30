abc2576715ccdff69b3b75b2c31c832bee72b58b
brandaoplaster <brandaoplaster@gmail.com>
2021-06-29 21:48:52 -0300

    chore: Adding migrate to user

---
+++ priv/repo/migrations/20210630004152_create_users_table.exs
@@ -0,0 +1,16 @@
+defmodule DailyMeals.Repo.Migrations.CreateUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create table :users do
+      add :name, :string
+      add :email, :string
+      add :cpf, :string
+
+      timestamps()
+    end
+
+    create unique_index(:users, [:cpf])
+    create unique_index(:users, [:email])
+  end
+end
