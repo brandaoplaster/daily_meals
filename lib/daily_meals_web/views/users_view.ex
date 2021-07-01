29d92ab2c05a3044fa8c68c97f4e9a3d38aefafa
brandaoplaster <brandaoplaster@gmail.com>
2021-06-30 22:42:20 -0300

    feat: Adding view user

---
+++ lib/daily_meals_web/views/users_view.ex
@@ -0,0 +1,17 @@
+defmodule DailyMealsWeb.UsersView do
+  use DailyMealsWeb, :view
+
+  alias DailyMeals.User
+
+  def render("create.json", %{user: %User{} = user}) do
+    %{
+      user: %{
+        id: user.id,
+        name: user.name,
+        cpf: user.cpf,
+        email: user.email
+      },
+      message: "User created!"
+    }
+  end
+end
