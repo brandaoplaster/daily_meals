1aff7cce1b1835dc18d7be03a181542107857e16
brandaoplaster <brandaoplaster@gmail.com>
2021-07-03 21:12:18 -0300

    chore: Adding tests to action show

--- test/daily_meals_web/controllers/users_controller_test.exs
+++ test/daily_meals_web/controllers/users_controller_test.exs
@@ -43,4 +43,38 @@ defmodule DailyMeals.UsersControllerTest do
       assert response == expected_response
     end
   end
+
+  describe "get/2" do
+    test "when id exist, return the meal", %{conn: conn} do
+      params = build(:user_params)
+
+      {_ok, user} = DailyMeals.create_user(params)
+
+      response =
+        conn
+        |> get(Routes.users_path(conn, :show, user.id))
+        |> json_response(:ok)
+
+      expected_response = %{
+        "user" => %{
+          "cpf" => "12345678901",
+          "email" => "joe@gmail.com",
+          "name" => "Joe"
+        }
+      }
+
+      assert expected_response = response
+    end
+
+    test "when id not exist, return an error", %{conn: conn} do
+      id = "5e694bc0-78fc-4600-bcd0-0733b7540a6e"
+
+      response =
+        conn
+        |> get(Routes.users_path(conn, :update, id))
+        |> json_response(:not_found)
+
+      assert %{"message" => "User not found"} = response
+    end
+  end
 end
